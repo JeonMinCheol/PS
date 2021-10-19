8ac1130e3beddf9a86f4572b9782065a82065066
JumpWithJmc0504 <sdkyu12345@gmail.com>
2021-10-19 15:42:48 +0900

    탐색 방법 고치기

--- 토마토.cpp
+++ 토마토.cpp
@@ -1,79 +1,80 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
-int n, m;
-int cnt = 0;
-int box[1001][1001];
-bool visited[1001][1001];
-int dx[4] = { 1, -1, 0, 0 };
-int dy[4] = { 0, 0, 1, -1 };
-void dfs(int x, int y) {
-	visited[x][y] = true;
-	cnt++;
+int n, m, k;
+int map[1001][1001];
+pair<int, int>p[4] = { {1,0}, {-1,0},{0,1},{0,-1} };
+void bfs(int x, int y) {
+	queue<pair<int, int>> q;
+	q.push({ x,y });
 
-	for (int i = 0; i < 4; i++) {
-		int nx = x + dx[i];
-		int ny = y + dy[i];
+	while (!q.empty()) {
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
 
-		if (nx < 1 || ny < 1 || 1000 < nx || 1000 < ny) {
-			continue;
-		}
-		if (!visited[nx][ny]) {
-			dfs(nx, ny);
-		}
-	}
-}
+		for (int i = 0; i < 4; i++) {
+			int row = x + p[i].first;
+			int col = y + p[i].second;
 
-int check() {
-	for (auto& row : box) {
-		for (auto col : row) {
-			if (col == 0) return 0;
+			if (row < 1 || col < 1 || n < row || m < col) {
+				continue;
+			}
+			else if (map[row][col] == 0) {
+				map[row][col] = map[x][y] + 1;
+				q.push({ row,col });
+				k = map[row][col];
+			}
 		}
 	}
-	return 1;
 }
 
 int main() {
-	cin >> n >> m;
-	int x = 0, y=0;
-	for (int i = 1; i < n; i++) {
-		for (int j = 1; j < m; j++) {
-			cin >> box[i][j];
-			visited[i][j] = false;
-			cnt++;
+	cin >> m >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			cin >> map[i][j];
 
-			if (box[i][j] == -1 || box[i][j] == 1) visited[i][j] = true;
 		}
 	}
 
-	if (check()) {
+	int cnt = 0;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (map[i][j] != 0) {
+				cnt++;
+			}
+		}
+	}
+
+	if (cnt == n * m) {
 		cout << 0;
 		return 0;
 	}
 
-
-	for (auto& row : box) {
-		for (auto col : row) {
-			if (col == 0) {
-				dfs(x, y);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (map[i][j] != 0 && map[i][j] != -1) {
+				bfs(i, j);
 			}
-			y++;
 		}
-		x++;
-		y = 0;
 	}
 
-	
-
-	for (auto& row : visited) {
-		for (auto col : row) {
-			if (col == false) {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			cout << map[i][j] << " ";
+		}
+		cout << endl;
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (map[i][j] == 0) {
 				cout << -1;
 				return 0;
 			}
 		}
 	}
 
-	cout << cnt;
-	return 0;
+	cout << k;
 }
