2c265263a9ee63e7fb21c6470634d736e668fca4
JumpWithJmc0504 <92536620+JumpWithJmc0504@users.noreply.github.com>
2021-12-18 15:01:23 +0900

    2352 / 반도체 설계

---
+++ 반도체 설계.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int n;
+int search(vector<int>& v) {
+	vector<int>q;
+	int cnt = 1;
+
+	q.push_back(v[0]);
+	for (int i = 1; i < n; i++) {
+		int start = 0, end = q.size();
+		while (start < end) {
+			int mid = (start + end) / 2;
+			if (q[mid] < v[i]) start = mid + 1;
+			else end = mid;
+		}
+		
+		if (end == q.size()) {
+			q.push_back(v[i]);
+		}
+		else {
+			q[end] = v[i];
+		}
+	}
+	return q.size();
+}
+
+int main() {
+	cin >> n;
+	vector<int>v(n);
+	for (int i = 0; i < n; i++) cin>>v[i];
+	cout << search(v);
+
+	return 0;
+}
