c4166b3bf6ed5f3bbf77b5a8408b2a32b7c88eec
JumpWithJmc0504 <92536620+JumpWithJmc0504@users.noreply.github.com>
2022-01-12 09:46:39 +0900

    1520 / 내리막 길

--- 내리막 길.cpp
+++ 내리막 길.cpp
@@ -1,42 +1,30 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
-int row, col;
-int map[500][500];
-int dp[500][500];
-int dcol[4] = { 1,-1,0,0 };
-int drow[4] = { 0,0,-1,1 };
-int solve(int r, int c) {
-	int& ret = dp[r][c];
-	if (r == row - 1 && c == col - 1) return 1;
-
-	
-	if (ret != 0) return ret;
-	
-	for (int i = 0; i < 4; i++) {
-		int nr = r + drow[i];
-		int nc = c + dcol[i];
-
-		if(nr < 0 || nc < 0 || row <= nr || col <= nc) continue;
-		if (map[nr][nc] >= map[r][c]) continue;
-		ret += solve(nr, nc);
-	}
-	return ret;
-}
+//물품의 수 ,버틸 수 있는 무게
+int n, k;
+int coin[100];
+int dp[10001];
 
 int main() {
-	cin >> row >> col;
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			cin >> map[i][j];
+	cin >> n >> k;
+	fill_n(dp, k + 1, 1000000001);
+	for (int i = 0; i < n; i++) {
+		cin >> coin[i];
+	}
+
+	for (int i = 0; i < n; i++) {
+		int c = coin[i];
+		for (int j = 1; j <= k; j++) {
+			if (j == c) dp[j] = 1;
+			else if (j > c) dp[j] = min(dp[j],dp[j - c] + 1);
 		}
 	}
-	solve(0,0);
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			cout << dp[i][j] << " ";
-		}cout<<"\n";
-	}
-	cout << dp[0][0];
+
+	
+	if(dp[k] == 1000000001) cout<<-1;
+	else cout << dp[k];
+	
 }
