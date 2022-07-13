963108e1983d68291bffb027ea76108afa1468d3
JumpWithJmc0504 <92536620+JumpWithJmc0504@users.noreply.github.com>
2022-07-13 19:00:55 +0900

    1987 / 알파벳

--- 알파벳.py
+++ 알파벳.py
@@ -2,17 +2,22 @@ import sys
 sys.setrecursionlimit(10**5)
 input = sys.stdin.readline
 
+ret = 0
 graph = []
 visit = [False] * 26
-r,c = map(int,input().split())
 dir = [[1,0],[-1,0],[0,1],[0,-1]]
+r,c = map(int,input().split())
 
-ret = 0
+for i in range(r):
+    graph.append(input())
+
+
 def dfs(row, col, cnt):
     global ret
     currentVal = graph[row][col]
     visit[ord(currentVal) - 65] = True
-    ret = max(ret, cnt )
+    if ret < cnt:
+        ret = cnt
 
     for i in range(4):
         newRow = row + dir[i][0]
@@ -24,9 +29,6 @@ def dfs(row, col, cnt):
                 dfs(newRow,newCol,cnt + 1)
                 visit[nextIndex] = False
 
-
-
-for i in range(r):
-    graph.append(input())
 dfs(0,0,1)
 print(ret)
+
