e4ea416573f23408599d10b856e32a5cdc7220e5
JumpWithJmc0504 <92536620+JumpWithJmc0504@users.noreply.github.com>
2022-07-21 11:17:36 +0900

    2146

---
+++ 다리만들기.py
@@ -0,0 +1,59 @@
+import sys
+from collections import deque
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+# 각 대륙별로 가장자리를 구하고 분류한다.
+def findEdge(row,col):
+    global n, visit, graph
+    ret = set()
+    dq = deque()
+    dq.append([row,col])
+    visit[row][col] = True
+    
+    dir = [[1,0],[-1,0],[0,1],[0,-1]]
+    while(len(dq)):
+        land = dq.pop()
+        for i in range(4):
+            newRow = land[0] + dir[i][0]
+            newCol = land[1] + dir[i][1]
+            if 0 <= newRow < n and 0 <= newCol < n:
+                if visit[newRow][newCol] == False:
+                    if graph[newRow][newCol] == 0:
+                        ret.add((land[0],land[1]))
+                    else:
+                        dq.append([newRow,newCol])
+                        visit[newRow][newCol] = True
+    return ret
+
+def dfs(row, col, cnt):
+# 매번 dfs해가면서 가장 짧은 거리 찾을 거임.ㅇ
+
+n = int(input())
+visit = [[False] * n for _ in range(n)]
+graph = []
+edge = []
+
+for i in range(n):
+    graph.append(list(map(int,input().split())))
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1 and visit[i][j] != True:
+            edge.append(findEdge(i,j))
+
+ret = 99999999
+for i in range(len(edge)):
+    # 제출시 지울것!!!!!  
+    print("%s: %s"%(i+1,edge[i]))
+    visit = [[False] * n for _ in range(n)]
+    
+    arr = list(edge[i])
+    for a in arr:
+        # 같은 대륙에 방문하지 못하도록 방문처리
+        for j in arr:
+            row = j[0], col = j[1]
+            visit[row][col] = True
+        #dfs(*)
+
+    
