441b364c77ab5364de1d1f5398f9097260cf72a9
JumpWithJmc0504 <92536620+JumpWithJmc0504@users.noreply.github.com>
2022-08-07 21:46:47 +0900

    11066

--- 파일 합치기.py
+++ 파일 합치기.py
@@ -1,27 +1,20 @@
-from collections import deque
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 t = int(input())
-for _ in range(t):
-    ret = 0
-    k = int(input())
-    arr = deque(map(int,input().split()))
 
-    while len(arr) > 2:
-        while len(arr) >= 3:
-            a = arr.popleft()
-            b = arr.popleft()
-            # arr의 원소가 1개 남거나 (그러면 옆에 있는 DP와 더함.) 인접 DP와 더한 것이 인접 원소와 더한 것 보다 크다면. (등호는 모르것다)
-            comp = 0
-            if len(arr) == 0 or a > arr[0]:
-                comp = a + b
-                arr.appendleft(comp)
-            else:
-                comp = arr.popleft() + b
-                arr.appendleft(comp)
-                arr.appendleft(a)
-            print(arr)
-            ret += comp
-    ret += arr[0] + arr[1]
-    print(ret)
+for _ in range(t):
+    n = int(input())
+    dp = [[0] * (n+1) for _ in range(n+1)] 
+    arr = deque(map(int,input().split()))
+    arr.appendleft(0)
+    arr = list(arr)
+    
+    for i in range(1,n+1):
+        for j in range(1,n+1-i):
+            ret = 987654321
+            for k in range(j,i+j):
+                ret = min(ret,dp[j][k] + dp[k+1][i+j])
+            dp[j][i + j] = sum(arr[j:i+j+1]) + ret
+    print(dp[1][n])
